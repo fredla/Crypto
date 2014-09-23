ea96109033f296ec91db0ca1a90ccf648640a658
fred <f@aol.com>
2014-09-23 16:27:23 +0200

    Correction fonctionnement correct pour tout type de caractere

--- Crypto/chiffreDecalage.cpp
+++ Crypto/chiffreDecalage.cpp
@@ -7,62 +7,71 @@ class ChiffrementDecal{
 
 public:
 
-  // Constructeur prenant le decalage voulu en argument
-  ChiffrementDecal(int decalage)
-  { 
-	  
-     // A vous de jouer...
-	  m_decalage=decalage;
-  }
+	// Constructeur prenant le decalage voulu en argument
+	ChiffrementDecal(int decalage)
+	{ 
+
+		// A vous de jouer...
+		m_decalage=decalage;
+	}
 
 
-  string crypt(string text)
-  {
-	  string textcrypt("");
-	   const char * c2;
-	   c2=text.c_str();
-	  for(int i=0;i<text.size();i++)
-	  {
-		  int decalage= (c2[i]+m_decalage)-65;
-		  int reste=decalage%26;
-		  char ch=char(65+reste);
-		  
-		textcrypt=textcrypt+ch;
-	  }
-	  return textcrypt;
-  }
-  // A vous de jouer...
+	string crypt(string text)
+	{
+		string textcrypt("");
+		const char * c2;
+		c2=text.c_str();
+		for(int i=0;i<text.size();i++)
+		{
+			char ch;
+			int ich=c2[i];
+			if(65<=ich && ich<=90)
+			{
+
+				int decalage= (c2[i]+m_decalage)-65;
+				int reste=decalage%26;
+
+				ch =char(65+reste);
+			}
+			else
+				ch=c2[i];
+
+			textcrypt=textcrypt+ch;
+		}
+		return textcrypt;
+	}
+	// A vous de jouer...
 
 private:
- 
-  int m_decalage; // Le decalage a appliquer au texte
-  
+
+	int m_decalage; // Le decalage a appliquer au texte
+
 };
 
 
 int main()
 {
-  
-  // Le message a crypter
-  string texte("BIENVENUE SUR LE MOOC C++ D'OCR !!");
 
-  // Demande du decalage a l'utilisateur
-  cout << "Quel decalage voulez-vous utiliser ? ";
-  int decalage;
-  cin >> decalage;
+	// Le message a crypter
+	string texte("BIENVENUE SUR LE MOOC C++ D'OCR !!");
 
-  // Creation du foncteur
-  ChiffrementDecal foncteur(decalage);
+	// Demande du decalage a l'utilisateur
+	cout << "Quel decalage voulez-vous utiliser ? ";
+	int decalage;
+	cin >> decalage;
 
-  // Chaine de caracteres pour le message crypter
-  string texte_crypte;
+	// Creation du foncteur
+	ChiffrementDecal foncteur(decalage);
 
-  texte_crypte=foncteur.crypt(texte);
+	// Chaine de caracteres pour le message crypter
+	string texte_crypte;
 
-  cout<<texte_crypte<<endl;
-  // A vous de jouer.... 
+	texte_crypte=foncteur.crypt(texte);
 
-  
-  return 0;
+	cout<<texte_crypte<<endl;
+	// A vous de jouer.... 
+
+	system("PAUSE");
+	return 0;
 }
 
